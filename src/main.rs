05d5c828634fd4eafb8fc36860d7fc9c809f0316
ritsource <ritwiksahaofficial@gmail.com>
2020-04-03 10:16:51 +0530

    Hello world!

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+extern crate gtk;
+use gtk::prelude::*;
+use gtk::{ButtonsType, DialogFlags, MessageDialog, MessageType, Window};
+
+fn main() {
+    if gtk::init().is_err() {
+        println!("Failed to initialize GTK.");
+        return;
+    }
+    MessageDialog::new(
+        None::<&Window>,
+        DialogFlags::empty(),
+        MessageType::Info,
+        ButtonsType::Ok,
+        "Hello World",
+    )
+    .run();
+}
